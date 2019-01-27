bd4b8a64934538b760d1751069f6ffd379873f2c
Andrew Crenwelge <andrewcrenwelge@gmail.com>
2019-01-26 19:20:28 -0500

    add readme

---
+++ README.md
@@ -0,0 +1,2 @@
+# Expense Reimbursement System
+A simple expense reimbursement system (ERS) - users can login, submit reimbursement requests, and see a history of submissions and whether they have been approved or not
--- src/com/ers/util/LogUtil.java
+++ src/com/ers/util/LogUtil.java
@@ -1,10 +1,10 @@
 package com.ers.util;
 
-import org.apache.log4j.Logger;
-//import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /* Final utility class containing root logger declaration used across the project */
 public final class LogUtil {
 
-	public final static Logger logger = Logger.getRootLogger();
+	public static final Logger logger = LogManager.getRootLogger();
 }
